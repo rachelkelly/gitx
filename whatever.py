8cc4214eda3d6a6a4caf70de7314bb3741f19ecf
ifjorissen <ifjorissen@gmail.com>
2014-07-03 19:53:28 -0700

    included off-beat reference to an indie movie

--- whatever.py
+++ whatever.py
@@ -1,2 +1,9 @@
 for i in range(6):
-    print "Git is fun until things go wrong. Horribly, horribly wrong."
+	print "Git is fun until things go wrong. Horribly, horribly wrong."
+	if i%2 == 0:
+		print "cool beans"
+	else:
+		print "hot rice"
+
+
+
